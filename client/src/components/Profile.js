3f5fe99470933148c0166c7bb1ae86399caa8d0d
Nathalie <nathalie.kirch.nk@gmail.com>
2023-10-08 21:10:03 +0200

    display networth data on profile page (only in the console right now)

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,19 @@
+import { useEffect } from 'react'
+import axiosAuth from '../lib/axios'
+
 export default function Login() {
+  useEffect(() => {
+    async function getData(){
+      try {
+        const { data } = await axiosAuth.get('/api/networths/') // <---- Replace with your endpoint to test the proxy
+        console.log(data)
+      } catch (error) {
+        console.log(error.response.data)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <h1>PROFILE</h1>
   )
