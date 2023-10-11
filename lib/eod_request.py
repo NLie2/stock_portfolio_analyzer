c28f89ed93823149591702f671817c5009f09c17
Nathalie <nathalie.kirch.nk@gmail.com>
2023-10-11 11:17:19 +0200

    edit eod request file to incldue API key

--- lib/eod_request.py
+++ lib/eod_request.py
@@ -1,8 +1,7 @@
 import requests 
 
-API_key = "63aef4c401d202.68341572"
 
-r = requests.get(f"https://eodhd.com/api/eod/AAPL.US?fmt=json&&api_token=demo")
+r = requests.get(f"https://eodhd.com/api/eod/AAPL.US?fmt=json&&api_token=${API_key}")
 
 
 print(r.json())
