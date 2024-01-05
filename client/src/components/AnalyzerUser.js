21991b50c41c43ac1f7b646e4d659c25e191e43d
Nathalie <nathalie.kirch.nk@gmail.com>
2024-01-05 19:11:49 +0100

    change filename back. PUBLIC URL automatically is set the the public folder's URL

--- client/src/components/AnalyzerUser.js
+++ client/src/components/AnalyzerUser.js
@@ -27,8 +27,7 @@ export default function AnalyzerUser() {
         <li> 3 upload your file and hit the SUBMIT button </li>
       </ul>
 
-      <a className= "file-template" href={'client/public/TestBuchführung_example.csv'} download='TestBuchführung.csv'> Download File Template </a>
-      < CsvUpload
+      <a className="file-template" href={process.env.PUBLIC_URL + '/TestBuchführung_example.csv'} download='TestBuchführung.csv'> Download File Template </a>      < CsvUpload
         formData={formData}
         setFormData={setFormData}
         setTradeData={setTradeData}
